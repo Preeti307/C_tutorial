f19367d2ec8304a2ce6d0151df89d406570ba726
Preeti Khatri <preetikhatri307@gmail.com>
2025-03-21 21:05:53 +0530

    Update tempCodeRunnerFile.c

--- tempCodeRunnerFile.c
+++ tempCodeRunnerFile.c
@@ -1,17 +1,21 @@
-//intf("Enter int  Number a = ");
-    // scanf("%d",&a);
+#include<stdio.h>
+int main(){
+    int a,b;
+printf("Enter int  Number a = ");
+     scanf("%d",&a);
 
-    // printf("Enter  int Number b = ");
-    // scanf("%d",&b);
+    printf("Enter  int Number b = ");
+    scanf("%d",&b);
 
-    // printf("The sum of a+b is  = %d\n", a+b);
+    printf("The sum of a+b is  = %d\n", a+b);
 
-    // //For float variable:
-    // float c,d;
-    // printf("Enter float Number c = ");
-    // scanf("%f",&c);
+    //For float variable:
+    float c,d;
+    printf("Enter float Number c = ");
+    scanf("%f",&c);
 
-    // printf("Enter float Number d = ");
-    // scanf("%f",&d);
+    printf("Enter float Number d = ");
+    scanf("%f",&d);
 
-    // printf("The sum of c+d is = %f\n", c+d);
+    printf("The sum of c+d is = %f\n", c+d);
+}
